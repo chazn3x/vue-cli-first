27f14519ef710c1032df35b71c7e1f88314f8d3a
Giovanni <gio.a.98@gmail.com>
2021-12-10 14:02:10 +0100

    fine hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="hero">
+        <Title/>
+    </div>
+</template>
+
+<script>
+import Title from "./Title.vue";
+
+export default {
+    name: 'Hero',
+    components: {
+        Title
+    }
+}
+</script>
+
+<style scoped>
+    .hero {
+        width: 100%;
+        height: 100vh;
+        position: relative;
+        background-image: url(../assets/581998-5K-Highway.jpg);
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+</style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>La mia prima app con Vue CLI!</h1>
+    <h1>La mia prima app con Vue CLI</h1>
 </template>
 
 <script>
@@ -10,6 +10,15 @@ export default {
 
 <style scoped>
     h1 {
-        font-weight: 300;
+        font-weight: 600;
+        font-size: 3.5rem;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: rgb(251,31,163);
+        text-transform: uppercase;
+        text-align: center;
+        text-shadow: 0 0 .5rem rgb(120,205,183);
     }
 </style>
