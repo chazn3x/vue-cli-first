4368d4eee308bb3108e1911599d63d1cdefc03f0
Giovanni <gio.a.98@gmail.com>
2021-12-10 13:28:32 +0100

    fine titolo

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
+
+<style scoped>
+    h1 {
+        font-weight: 300;
+    }
+</style>
